a223641983d0bb3e82b26f420b160e21e8deb3dc
Robin Kumar <53229119+Robin325coder@users.noreply.github.com>
2022-02-27 11:06:46 +0530

    callback fns

---
+++ 12callbackfns.js
@@ -0,0 +1,30 @@
+// // callback function
+// /**
+//  * here y() is a callback function because
+//  * it is passed as an argument to x()
+//  */
+// function x(param) {
+
+// }
+
+// x(function y() {
+
+// });
+
+/**
+ * understanding asynchronous programming
+ * using setTimeout()
+ */
+setTimeout(function () {
+    console.log("timer");
+}, 5000);
+
+function x(param) {
+    console.log("in x()");
+    param();
+}
+
+x(function y() {
+    console.log("in y()");
+});
+
